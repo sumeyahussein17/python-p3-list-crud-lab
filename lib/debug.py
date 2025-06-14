72dd018ec2f393c7997191171ddbb5b242f75739
sumeyahussein17 <sumeyahussein21@gmail>
2025-06-14 02:39:32 -0700

    add

--- lib/debug.py
+++ lib/debug.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 from list_crud import (
     create_an_empty_list, create_a_list,
     add_element_to_end_of_list, add_element_to_start_of_list,
@@ -7,6 +5,5 @@ from list_crud import (
     retrieve_first_element_from_list, retrieve_element_from_index,
     retrieve_last_element_from_list
 )
-
 if __name__ == '__main__':
     import ipdb; ipdb.set_trace()
